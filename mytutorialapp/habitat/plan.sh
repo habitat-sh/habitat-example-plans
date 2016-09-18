c612bf9b0b2ae0ec674456445e3ca3a371fbe789
Nathen Harvey <nharvey@chef.io>
2016-09-17 23:06:12 -0400

    `pkg_source` is required
    The `pkg_source` is now required in the `plan.sh`.
    
    With `do_download()` overridden, this will not be used but is still
    required.
    
    An alternate value might be something like `nosuchfile.tar.gz`
    
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- mytutorialapp/habitat/plan.sh
+++ mytutorialapp/habitat/plan.sh
@@ -3,5 +3,6 @@ pkg_name=mytutorialapp
 pkg_version=0.2.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=()
+pkg_source=https://github.com/habitat-sh/habitat-example-plans
 pkg_deps=()
 pkg_expose=()
