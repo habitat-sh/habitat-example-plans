d8e0c40f13b283ac715da41ad8d08bfbcdfa5c77
Nathen Harvey <nharvey@chef.io>
2016-09-30 10:00:45 -0400

    Add `pkg_source` to the finished plan, too
    This is now a required field. The 'finished' tutorial should include the
    attribute, too.
    
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- mytutorialapp_finished/habitat/plan.sh
+++ mytutorialapp_finished/habitat/plan.sh
@@ -3,6 +3,7 @@ pkg_name=mytutorialapp
 pkg_version=0.2.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=()
+pkg_source=https://github.com/habitat-sh/habitat-example-plans
 pkg_deps=(core/node)
 pkg_expose=(8080)
 
