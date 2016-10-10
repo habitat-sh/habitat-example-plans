3772a74b09ebcbde686f000d8760559e4dea4d12
Sean Carolan <scarolan@gmail.com>
2016-10-10 14:22:08 -0700

    Added new tutorial that has API-driven images with adjustable height and width.

--- mytutorialapp_finished/source/server.js
+++ mytutorialapp_finished/source/server.js
@@ -1,27 +1,15 @@
 var http = require('http'),
-nconf = require('nconf');
+	nconf = require('nconf');
+
 
 nconf.file({ file: '../config/config.json' });
 
 var port = nconf.get('port');
 
-var body = ` 
-<html> 
-  <head>
-    <title>${nconf.get('message')}</title>
-  </head>
-  <body>
-    <center><h3>${nconf.get('message')}</h3></center>
-    <center><img src=${nconf.get('url')}/${nconf.get('width')}/${nconf.get('height')}>
-  </body>
-</html>
-`
-
 var handleRequest = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end;    
-};
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end(nconf.get('message'));    
+}
 
 var www = http.createServer(handleRequest);
 www.listen(port, function() {
